f2bfd21ca96b6f2767ee2008e63c7e4a82383510
wxue1 <xue1.wang@intel.com>
2023-03-20 02:07:55 -0700

    JIT deepest function firstly
    Duplicated JITTed code brings overhead for the instruction cache.
    This patch reduces duplication by JITting deepest inline function
    first because the same function is JITTed in different
    root trace or side trace sometimes.
    It increases 3% the performance of our workload in tracing mode.
    
    Signed-off-by: Wang, Xue   <xue1.wang@intel.com>
    Signed-off-by: Yang, Lin A <lin.a.yang@intel.com>

--- ext/opcache/jit/zend_jit_vm_helpers.c
+++ ext/opcache/jit/zend_jit_vm_helpers.c
@@ -1048,6 +1048,13 @@ zend_jit_trace_stop ZEND_FASTCALL zend_jit_trace_execute(zend_execute_data *ex, 
 
 		trace_flags = ZEND_OP_TRACE_INFO(opline, offset)->trace_flags;
 		if (trace_flags) {
+			/* When enter a function, stop current tracing and restart a new one */
+			if (trace_buffer[idx-1].op == ZEND_JIT_TRACE_ENTER) {
+				if (!(trace_flags & ZEND_JIT_TRACE_JITED)) {
+					stop = ZEND_JIT_TRACE_STOP_JIT_DEEPER_FUNC;
+					break;
+				}
+			}
 			if (trace_flags & ZEND_JIT_TRACE_JITED) {
 				if (trace_flags & ZEND_JIT_TRACE_START_LOOP) {
 					if ((start & ZEND_JIT_TRACE_START_LOOP) != 0
